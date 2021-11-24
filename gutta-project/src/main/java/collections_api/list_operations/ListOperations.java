a62c750d70f864a3a9e60138e80ef07dc852efb8
RamaKoteswararao Gutta <gutta.ramakoteswararao@gmail.com>
2021-11-24 16:58:45 +0530

    java datastructures template

---
+++ gutta-project/src/main/java/collections_api/list_operations/ListOperations.java
@@ -0,0 +1,10 @@
+package collections_api.list_operations;
+
+public class ListOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("List Operations");
+	}
+
+}
---
+++ gutta-project/src/main/java/collections_api/set_operations/SetOperations.java
@@ -0,0 +1,10 @@
+package collections_api.set_operations;
+
+public class SetOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Set Operations");
+	}
+
+}
